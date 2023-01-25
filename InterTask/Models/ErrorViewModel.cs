2b53281ec0151aac1fe82793fa5d870d98963cc0
Asror020 <114496649+Asror020@users.noreply.github.com>
2023-01-25 23:37:40 +0500

    Add files via upload

---
+++ InterTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InterTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
